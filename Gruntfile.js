7ea87fdcdf1fa1034a3d5a1213619e87cb819a58
eastkiki <eastkiki@hanmail.net>
2013-11-14 10:58:51 +0900

    Add grunt task sample

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,6 +32,16 @@ module.exports = function (grunt) {
                 "globals": {}
             },
             all: ['<%= scriptFiles %>']
+        },
+        css2js: {
+            onesrc_test: {
+                src: 'sample/one.css',
+                dest: 'dist/onesrc_test_css2js.js'
+            },
+            multisrc_test: {
+                src: ['sample/one.css', 'sample/two.css'],
+                dest: 'dist/multisrc_test_css2js.js'
+            }
         }
     });
 
@@ -39,6 +49,9 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-nodeunit');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    // Load css2js task
+    grunt.loadTasks('tasks');
 
     // Default task.
     grunt.registerTask('default', ['jshint', 'nodeunit']);
